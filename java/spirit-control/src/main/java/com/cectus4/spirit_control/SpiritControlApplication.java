d5c7589e6bf4762e2a3f70e89d093aa7f11619cd
CectAss <npnbem.fm@gmail.com>
2025-04-22 23:29:20 +0300

    some java garbage

---
+++ java/spirit-control/src/main/java/com/cectus4/spirit_control/SpiritControlApplication.java
@@ -0,0 +1,15 @@
+package com.cectus4.spirit_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritControlApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpiritControlApplication.class, args);
+
+	}
+
+}
---
+++ java/spirit-control/src/test/java/com/cectus4/spirit_control/SpiritControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cectus4.spirit_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiritControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

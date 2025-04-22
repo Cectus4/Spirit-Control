337cae8f9b1bdd3e18cfbc11fc7ebf3c681d13be
CectAss <npnbem.fm@gmail.com>
2025-04-22 20:33:34 +0300

    multifile support

--- python/ogg2wav.py
+++ python/ogg2wav.py
@@ -1,6 +1,6 @@
 import soundfile as sf
 import os
 
-data, samplerate = sf.read("python/ogg2wavInput/"+os.listdir("python/ogg2wavInput/")[0])
-
-sf.write('python/ogg2wavOutput/'+os.listdir("python/ogg2wavInput/")[0]+'.wav', data, samplerate)
+for file in os.listdir("python/ogg2wavInput/"):
+    data, samplerate = sf.read("python/ogg2wavInput/"+file)
+    sf.write('python/ogg2wavOutput/'+file+'.wav', data, samplerate)

762fd20638ee6cac5651ba6ab453e9d8272992e9
CectAss <npnbem.fm@gmail.com>
2025-04-24 00:56:04 +0300

    misc

--- java/spirit-control/src/main/java/com/cectus4/spirit_control/controller/UserController.java
+++ java/spirit-control/src/main/java/com/cectus4/spirit_control/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/spirit")
 @AllArgsConstructor
-public class SpiritController {
+public class UserController {
 
     private final UserService spiritService;
 
---
+++ java/spirit-control/src/main/java/com/cectus4/spirit_control/service/impl/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.cectus4.spirit_control.service.impl;
+
+public class UserServiceImpl {
+}

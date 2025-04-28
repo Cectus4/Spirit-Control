548e2e3cfec636b5ae5c0ba43e0dee732eb5fe0b
CectAss <npnbem.fm@gmail.com>
2025-04-28 22:27:18 +0300

    embed v

--- python/asmrbd/moderation.py
+++ python/asmrbd/moderation.py
@@ -4,4 +4,4 @@ db = db()
 db.create()
 
 while True:
-    db.query("insert into asmr (url) values ('{0}');".format(input()))
+    db.query("insert into asmr (url) values ('{0}');".format("https://www.youtube.com/embed/"+input().split("v=")[1]))

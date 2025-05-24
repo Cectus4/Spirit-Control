f8cd50ec303aa15a8273e808543ecccf764caabc
CectAss <npnbem.fm@gmail.com>
2025-05-24 16:25:45 +0300

    generate tables

--- java/spirit-control/src/main/java/com/cectus4/spirit_control/controllers/EmoStatisticsController.java
+++ java/spirit-control/src/main/java/com/cectus4/spirit_control/controllers/EmoStatisticsController.java
@@ -1,10 +1,12 @@
 package com.cectus4.spirit_control.controllers;
 
+import com.alibaba.excel.EasyExcel;
 import com.cectus4.spirit_control.model.EmoStatistics;
 import com.cectus4.spirit_control.service.EmoStatisticsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -33,6 +35,17 @@ public class EmoStatisticsController {
     @GetMapping("find-by-user-id/{userId}")
     public List<EmoStatistics> getEmoStatisticsByUserId(@PathVariable long userId) {
         return emoStatisticsService.findEmoStatisticsByUserId(userId);
+    }
+
+    @GetMapping("get-user-calendar/{userId}")
+    public String getUserCalendar(@PathVariable long userId) {
+        List<EmoStatistics> emoStatisticsList = emoStatisticsService.findEmoStatisticsByUserId(userId);
+
+        EasyExcel.write(String.valueOf(userId)+"Statistics.xlsx", EmoStatistics.class)
+                .sheet("Статистика")
+                .doWrite(emoStatisticsList);
+
+        return "success";
     }
 
 }
--- java/spirit-control/src/main/java/com/cectus4/spirit_control/model/EmoStatistics.java
+++ java/spirit-control/src/main/java/com/cectus4/spirit_control/model/EmoStatistics.java
@@ -1,7 +1,11 @@
 package com.cectus4.spirit_control.model;
 
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.write.style.HeadStyle;
 import jakarta.persistence.*;
 import lombok.Data;
+import com.alibaba.excel.enums.poi.FillPatternTypeEnum;
 
 import java.time.LocalDate;
 
@@ -11,11 +15,17 @@ import java.time.LocalDate;
 public class EmoStatistics {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ExcelIgnore
     private long id;
     @Column(nullable = false)
+    @ExcelIgnore
     private long userId;
     @Column(nullable = false, length = 1024)
+    @ExcelProperty("Эмоция")
+    @HeadStyle(fillPatternType = FillPatternTypeEnum.SOLID_FOREGROUND, fillForegroundColor = 9)
     private String emoDict;
+    @ExcelProperty("Дата")
     @Column(nullable = false)
+    @HeadStyle(fillPatternType = FillPatternTypeEnum.SOLID_FOREGROUND, fillForegroundColor = 9)
     private LocalDate date;
 }
